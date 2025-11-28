6ebb49bf9b4a7a35aef40d7832e01ac728652d72
vivek248 <vivekpratap181@gmail.com>
2025-11-29 03:27:16 +0530

    change to h2

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -5,7 +5,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h2>{title}</h3>
+      <h2>{title}</h2>
       <p>{text}</p>
     </article>
   );
